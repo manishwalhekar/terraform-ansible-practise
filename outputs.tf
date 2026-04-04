1ce8f036e49aa4854581fecec905fb61f0dfdd17
manishwalhekar <manish.walhekar10@gmail.com>
2026-04-04 19:14:02 +0000

    updated output file with name and public ip

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,8 @@
 output "instance_ip_addr" {
   value       = [
-    for instance in aws_instance.my_instance : instance.public_ip
+    for instance in aws_instance.my_instance : {
+      name = instance.tags.Name
+      public_ip = instance.public_ip}
   ]
   description = "public ip of all 4 instance"
 }
